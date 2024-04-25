433628811e5f5d48c742d46f8fedf1e325e2f1bc
Jeremy Yun <113321384+JermYeWorm@users.noreply.github.com>
2024-04-25 16:20:52 -0400

    Update Clock_Enable.v

--- Clock_Enable.v
+++ Clock_Enable.v
@@ -10,7 +10,7 @@ module Clock_Enable(
     
     always @(posedge clk or posedge clr)
         if (clr == 1) begin
-            count <= 0;         // ???
+            count <= 0;
             clk_en <= 0;
         end
         else if (count == 99999) begin
